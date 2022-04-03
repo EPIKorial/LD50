1b3c3819df252153ff5ff81befc12e2f40b3ee8e
Florian Huet <florian.huet@ixxitechside.com>
2022-04-03 21:24:29 +0200

    Added infinite segment loading

--- Scripts/ScrollingSegment.gd
+++ Scripts/ScrollingSegment.gd
@@ -1,14 +1,34 @@
-extends Sprite
+extends Node2D
 
 var jumpPosition = 615
 
 export var speed = 1
 
+var bgs = []
+
 func _ready():
-	pass # Replace with function body.
+	bgs.append(get_node("Background"))
+	bgs.append(get_node("Background2"))
 
 func _process(delta):
-	position.y += speed
+	if bgs.front() == null || bgs.back() == null:
+		return
+	bgs.front().position.y += speed
+	bgs.back().position.y += speed
 	
-	if position.y >= jumpPosition:
-		position.y = -jumpPosition
+	if bgs.front().position.y >= jumpPosition:
+		bgs.front().position.y = -jumpPosition
+		var newBG = load("res://Prefabs/Background2.tscn").instance()
+		newBG.position = bgs.front().position
+		bgs.append(newBG)
+		add_child(newBG)
+		if bgs.size() > 2:
+			bgs.remove(0)
+	if bgs.back().position.y >= jumpPosition:
+		bgs.back().position.y = -jumpPosition
+		var newBG = load("res://Prefabs/Background2.tscn").instance()
+		newBG.position = bgs.back().position
+		bgs.append(newBG)
+		add_child(newBG)
+		if bgs.size() > 2:
+			bgs.remove(1)
