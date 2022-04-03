b3042d436b672f127500a9b49c9e2ae644ae2ebd
Florian Huet <florian.huet@ixxitechside.com>
2022-04-03 23:33:28 +0200

    Added new segments

--- Scripts/ScrollingSegment.gd
+++ Scripts/ScrollingSegment.gd
@@ -5,8 +5,13 @@ var jumpPosition = 615
 export var speed = 1
 
 var bgs = []
+var bgPathRoot = "res://Prefabs/Background"
+var bgPathEnd = ".tscn"
+
+onready var rng = RandomNumberGenerator.new()
 
 func _ready():
+	rng.randomize()
 	bgs.append(get_node("Background"))
 	bgs.append(get_node("Background2"))
 
@@ -16,9 +21,11 @@ func _process(delta):
 	bgs.front().position.y += speed
 	bgs.back().position.y += speed
 	
+	var rand = str(rng.randi_range(1, 5))
+	
 	if bgs.front().position.y >= jumpPosition:
 		bgs.front().position.y = -jumpPosition
-		var newBG = load("res://Prefabs/Background2.tscn").instance()
+		var newBG = load(bgPathRoot+rand+bgPathEnd).instance()
 		newBG.position = bgs.front().position
 		bgs.append(newBG)
 		add_child(newBG)
@@ -26,7 +33,7 @@ func _process(delta):
 			bgs.remove(0)
 	if bgs.back().position.y >= jumpPosition:
 		bgs.back().position.y = -jumpPosition
-		var newBG = load("res://Prefabs/Background2.tscn").instance()
+		var newBG = load(bgPathRoot+rand+bgPathEnd).instance()
 		newBG.position = bgs.back().position
 		bgs.append(newBG)
 		add_child(newBG)
